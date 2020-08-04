6c7e441ee5ed01432d78a079bf4d8cf08f3bd988
jaradat-pdb <admin@pdbcorp.org>
2020-08-03 23:41:00 -0400

    Add unit test classes for services

---
+++ uni-eap/src/test/java/org/pdbcorp/eap/uni/service/impl/AddressDetailsServiceTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2020 PDB Corp.
+ *
+ * Proprietary Software built off of open-source software?
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.pdbcorp.eap.uni.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.pdbcorp.eap.uni.data.model.Address;
+import org.pdbcorp.eap.uni.data.repo.AddressRepository;
+import org.pdbcorp.eap.uni.util.TestDataFactoryUtil;
+
+/**
+ * 
+ * @author jaradat-pdb
+ */
+@ExtendWith(MockitoExtension.class)
+class AddressDetailsServiceTest {
+
+	@Mock
+	private AddressRepository addressRepository;
+
+	@InjectMocks
+	private AddressDetailsService service = new AddressDetailsService(addressRepository);
+
+	@DisplayName("Successfully query ADDRESS nodes using ADDR_LINE_1")
+	@Test
+	void validFindByAddrLine1Test() throws Exception {
+		Address expected = TestDataFactoryUtil.generateAddressInstance();
+		when(addressRepository.findByAddrLine1(expected.getAddrLine1())).thenReturn(expected);
+		Address actual = service.findByAddrLine1(expected.getAddrLine1());
+		assertEquals(expected, actual);
+	}
+
+}
---
+++ uni-eap/src/test/java/org/pdbcorp/eap/uni/service/impl/PersonDetailsServiceTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2020 PDB Corp.
+ *
+ * Proprietary Software built off of open-source software?
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.pdbcorp.eap.uni.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.pdbcorp.eap.uni.data.model.Person;
+import org.pdbcorp.eap.uni.data.repo.PersonRepository;
+import org.pdbcorp.eap.uni.util.TestDataFactoryUtil;
+
+/**
+ * 
+ * @author jaradat-pdb
+ */
+@ExtendWith(MockitoExtension.class)
+class PersonDetailsServiceTest {
+
+	@Mock
+	private PersonRepository personRepository;
+
+	@InjectMocks
+	private PersonDetailsService service = new PersonDetailsService(personRepository);
+
+	@DisplayName("Successfully save a PERSON node via the details service")
+	@Test
+	void validSavePersonTest() throws Exception {
+		Person expected = TestDataFactoryUtil.generatePersonInstance();
+		when(personRepository.save(expected)).thenReturn(expected);
+		assertEquals(expected, service.savePerson(expected));
+	}
+
+}
