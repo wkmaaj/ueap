b16e5564a361f9d048f883e9f9d437071da7c8f8
jaradat-pdb <admin@pdbcorp.org>
2020-08-08 18:34:37 -0400

    Update method to generate propsUid for input entity and to then compare with db returned entities to determine if unique or not

--- uni-eap/src/main/java/org/pdbcorp/eap/uni/service/validation/impl/PersonValidationService.java
+++ uni-eap/src/main/java/org/pdbcorp/eap/uni/service/validation/impl/PersonValidationService.java
@@ -21,6 +21,7 @@ import java.util.Collection;
 
 import org.pdbcorp.eap.uni.data.model.Person;
 import org.pdbcorp.eap.uni.data.repo.PersonRepository;
+import org.pdbcorp.eap.uni.service.generation.impl.PersonUniqueStringGenerationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -35,19 +36,45 @@ import lombok.extern.slf4j.Slf4j;
 public class PersonValidationService {
 
 	private PersonRepository personRepository;
+	private PersonUniqueStringGenerationService personUniqueStringGenerationService;
 
 	@Autowired
-	public PersonValidationService(PersonRepository personRepository) {
+	public PersonValidationService(
+		PersonRepository personRepository, PersonUniqueStringGenerationService personUniqueStringGenerationService) {
+		
 		this.personRepository = personRepository;
+		this.personUniqueStringGenerationService = personUniqueStringGenerationService;
 	}
 
-	public boolean validateSavePerson(Person person) {
-		Collection<Person> dbPeople = personRepository.
-				findByFnameAndMnameAndLname(person.getFname(), person.getMname(), person.getLname());
-		if(dbPeople.size() > 0) {
-			return true;
+	/**
+	 * This method queries the db via the 
+	 * {@link org.pdbcorp.eap.uni.data.repo.PersonRepository#findByFnameAndMnameAndLname(String, String, String) findByFnameAndMnameAndLname}
+	 * query method for any existing nodes that contain the same <code>fname</code>, <code>mname</code>, and <code>lname</code>
+	 * field properties.<br>
+	 * If the query returns a collection, then the {@link org.pdbcorp.eap.uni.data.model.Person#getPropsUid() propsUid} 
+	 * of the input parameter object and the returned nodes are compared to see if equivalent. If a returned node is found
+	 * to have an equivalent {@link org.pdbcorp.eap.uni.data.model.Person#getPropsUid() propsUid}, then that node is returned
+	 * to the method caller. Else, the input parameter object is passed back to the method caller with its
+	 * {@link org.pdbcorp.eap.uni.data.model.Person#getPropsUid() propsUid} property set.
+	 * 
+	 * @param person - the object to check the db for.
+	 * @return Person - either the initial object passed in as a parameter, or the PERSON node currently present in the db.
+	 */
+	public Person validateExists(Person person) {
+		person.setPropsUid(personUniqueStringGenerationService.generateUniqueIdString(person));
+		Collection<Person> dbNodes = personRepository
+			.findByFnameAndMnameAndLname(person.getFname(), person.getMname(), person.getLname());
+		if(!dbNodes.isEmpty()) {
+			for(Person dbNode : dbNodes) {
+				if(person.getPropsUid().equals(dbNode.getPropsUid())) {
+					if(log.isDebugEnabled()) {
+						log.debug("Identified existing node with matching propsUid: {}", dbNode);
+					}
+					return dbNode;
+				}
+			}
 		}
-		return false;
+		return person;
 	}
 
 }
