bfc8d509befc3c78d6b4ebd8e549a38ca3aed0d3
wkmaaj <wkmaaj@pdbcorp.org>
2021-07-25 21:37:06 -0700

    Add Email, GitHub, and Twitter authentication providers

--- uni-eap-auth/pages/api/auth/[...nextauth].js
+++ uni-eap-auth/pages/api/auth/[...nextauth].js
@@ -3,9 +3,28 @@ import Providers from "next-auth/providers";
 
 export default NextAuth({
   providers: [
+    Providers.Email({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
     Providers.Google({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    Providers.Twitter({
+      clientId: process.env.TWITTER_ID,
+      clientSecret: process.env.TWITTER_SECRET,
     }),
   ],
   database: process.env.MONGODB_URI,
