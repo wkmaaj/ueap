d713a53611aefa514982aa5b5a0c1675cf0daf7a
wkmaaj <wkmaaj@pdbcorp.org>
2021-07-28 11:23:14 -0700

    Add React Head element and parent div wrapper (to address one component per JSX requirement)

--- uni-eap-auth/pages/index.js
+++ uni-eap-auth/pages/index.js
@@ -1,49 +1,62 @@
 import { signIn, signOut, useSession } from "next-auth/client";
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const [session, loading] = useSession();
   return (
-    <main className={styles.main}>
-      <div>
-        {loading && <p>Loading...</p>}
-        {!session && (
-          <>
-            Not signed in <br />
-            <br />
-            <button
-              onClick={() =>
-                signIn("github", { callbackUrl: "http://localhost:3000/" })
-              }
-            >
-              Sign In with GitHub
-            </button>
-            <br />
-            <button
-              onClick={() =>
-                signIn("google", { callbackUrl: "http://localhost:3000/" })
-              }
-            >
-              Sign In with Google
-            </button>
-            <br />
-            <button
-              onClick={() =>
-                signIn("twitter", { callbackUrl: "http://localhost:3000/" })
-              }
-            >
-              Sign In with Twitter
-            </button>
-          </>
-        )}
-        {session && (
-          <>
-            Signed in as {session.user.name}
-            <br />
-            <button onClick={() => signOut()}>Sign Out</button>
-          </>
-        )}
-      </div>
-    </main>
+    <div className={styles.container}>
+      <Head>
+        <title>UEAP</title>
+        <link rel="icon" href="/favicon.iso" />
+      </Head>
+      <main className={styles.main}>
+        <div>
+          {loading && <p>Loading...</p>}
+          {!session && (
+            <>
+              Not signed in <br />
+              <br />
+              <button
+                onClick={() =>
+                  signIn("github", { callbackUrl: "http://localhost:3000/" })
+                }
+              >
+                Sign In with GitHub
+              </button>
+              <br />
+              <button
+                onClick={() =>
+                  signIn("google", { callbackUrl: "http://localhost:3000/" })
+                }
+              >
+                Sign In with Google
+              </button>
+              <br />
+              <button
+                onClick={() =>
+                  signIn("twitter", { callbackUrl: "http://localhost:3000/" })
+                }
+              >
+                Sign In with Twitter
+              </button>
+            </>
+          )}
+          {session && (
+            <>
+              Signed in as {session.user.name}
+              <br />
+              <button>
+                <Link href="/secret">Secret</Link>
+              </button>
+              <br />
+              <button onClick={() => signOut()}>Sign Out</button>
+            </>
+          )}
+        </div>
+      </main>
+    </div>
   );
 }
