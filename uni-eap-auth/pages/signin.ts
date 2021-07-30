d087ad7b974bb2b13290169b18eca88b348d4674
wkmaaj <wkmaaj@pdbcorp.org>
2021-07-30 08:16:12 -0700

    Initial commit of custom app signin page

---
+++ uni-eap-auth/pages/signin.ts
@@ -0,0 +1,30 @@
+import {
+  providers,
+  signIn,
+  getSession,
+  csrfToken,
+  CtxOrReq,
+} from "next-auth/client";
+
+export default function SignIn({ providers, csrfToken }) {
+  return <Container></Container>;
+}
+
+SignIn.getInitialProps = async (context) => {
+  const { req, res } = context;
+  const session = await getSession({ req });
+  // check if user is already signed in
+  if (session && res && session.accessToken) {
+    // if signed in already, redirect user back to home
+    res.writeHead(302, {
+      Location: "/",
+    });
+    res.end();
+    return;
+  }
+  return {
+    session: undefined,
+    providers: await providers(),
+    csrfToken: await csrfToken(context),
+  };
+};
